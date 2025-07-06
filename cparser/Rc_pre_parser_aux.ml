190f98482cbb886c8b84551cba221284085deca2
Eli Whitehouse <eliw55@gmail.com>
2025-07-06 14:54:13 -0500

    insert types from `Rc_annot.ml` in refinedc/frontend

--- cparser/Rc_pre_parser_aux.ml
+++ cparser/Rc_pre_parser_aux.ml
@@ -34,3 +34,44 @@ type arguments =
   | Zero 
   | One of (Cabs.loc * string list)
   | Many of ((Cabs.loc * string list) list)
+
+type 'a quote_elt = 
+  | Plain of string 
+  | Anti of 'a
+
+type 'a quote = 'a quote_elt list
+
+type ident   = string
+type pattern = ident list
+
+type coq_term  = type_expr quote
+
+and  iris_term = type_expr quote
+
+and coq_expr =
+  | Coq_ident of string
+  | Coq_all   of coq_term
+
+and constr =
+  | Constr_Iris  of iris_term
+  | Constr_exist of string * coq_expr option * constr
+  | Constr_own   of string * ptr_kind * type_expr
+  | Constr_val   of string * type_expr
+  | Constr_Coq   of coq_expr
+  | Constr_glob  of string * type_expr
+
+and ptr_kind = Own | Shr | Frac of coq_expr
+
+and type_expr =
+  | Ty_refine of coq_expr * type_expr
+  | Ty_dots
+  | Ty_exists of pattern * coq_expr option * type_expr
+  | Ty_constr of type_expr * constr
+  | Ty_params of ident * type_expr_arg list
+  | Ty_Coq    of coq_expr
+
+and type_expr_arg =
+  | Ty_arg_expr   of type_expr
+  | Ty_arg_lambda of pattern * coq_expr option * type_expr_arg
+
+type annot_arg = int * int * coq_expr
